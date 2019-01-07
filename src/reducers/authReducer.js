5a8f9ac955d868c8f9a254557f436e189fe28222
Chiagoziem Nwadike <chiagoziemnwadike@Chiagoziems-MacBook-Pro.local>
2019-01-07 17:30:21 +0100

    [Feature #162152820] Login user.

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -4,6 +4,12 @@ const initialState = {
   user: {},
 };
 
+/**
+ * @description Request to the  API to signup a user
+ * @param {object} state - initial state
+ * @param {object} action - action dispatched
+ * @return {object} current state
+ */
 const AuthReducer = (state = initialState, action) => {
   switch (action.type) {
   case ADD_USER:
